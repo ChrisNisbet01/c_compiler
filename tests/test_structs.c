e02f588dd78d3ccd840511a1541699f88e632316
Chris Nisbet <chris@nisbet.kiwi.nz>
2026-03-20 04:36:07 +1300

    Add basic struct test case

--- tests/test_structs.c
+++ tests/test_structs.c
@@ -3,22 +3,7 @@ struct Point {
     int y;
 };
 
-struct Rectangle {
-    struct Point top_left;
-    int width;
-    int height;
-};
-
 int main() {
     struct Point p;
-    p.x = 10;
-    p.y = 20;
-    
-    struct Rectangle r;
-    r.top_left.x = 0;
-    r.top_left.y = 0;
-    r.width = 100;
-    r.height = 50;
-    
-    return p.x + p.y + r.width + r.height;
+    return 0;
 }
